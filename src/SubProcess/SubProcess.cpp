ee451e9fecdf877f70d649db564cf6a994a1e4a9
tabxx <2969117392@qq.com>
2025-11-14 20:37:25 +0800

    Canceled: CLI and Subprocess. This project only considers WebSocket communicating.

--- src/SubProcess/SubProcess.cpp
+++
@@ -1,1 +0,0 @@
-#include "SubProcess.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,11 @@
 #include <ThostFtdcTraderApi.h>
 
 #include "WebSocketApp.hpp"
-#include "SubProcess/SubProcess.hpp"
 
 using namespace tabxx;
 using std::string;
 
 struct execConfig {
-    enum {
-        WS, SUBPROCESS
-    } mode = WS;
     string addr = "localhost";
     string port = "8888";
     string flow = "./flow";
@@ -29,20 +25,13 @@ int main(int argc, char** args) {
         return ret;
     }
 
-    switch (config.mode) {
-    case execConfig::WS: try {
+    try {
         WebSocketApp app(config.addr, config.port, config.flow);
         app.run();
-        break;
+        return 0;
     } catch (const std::exception& e) {
         std::cerr << "Failed to initialize WebSocketApp: " << e.what() << std::endl;
         return 1;
-    }
-    case execConfig::SUBPROCESS: {
-        tabxx::SubProcess subProcess(config.flow);
-        subProcess.run();
-        break;
-    }
     }
 }
 
@@ -53,8 +42,6 @@ const char * hint =
 "  -v, --version    Display the version information and exit\n"
 "  -a, --addr       Specify the address to listen on (default: localhost)\n"
 "  -p, --port       Specify the port to listen on (default: 8888)\n"
-"  -m, --mode       Specify the mode to run in (default: ws)\n"
-"                   Supported modes: ws, subprocess"
 "  -f, --flow       Specify the flow directory (default: ./flow)";
 
 
@@ -83,24 +70,6 @@ int parseArgs(int argc, char** args, execConfig& config) {
         else if (arg == "-p" || arg == "--port") {
             if (i + 1 < argc) {
                 config.port = args[++i];
-            } else {
-                std::cerr << "Error: Option " << arg << " requires an argument." << std::endl;
-                std::cerr << hint << std::endl;
-                return 1;
-            }
-        }
-        else if (arg == "-m" || arg == "--mode") {
-            if (i + 1 < argc) {
-                string modeStr = args[++i];
-                if (modeStr == "ws") {
-                    config.mode = execConfig::WS;
-                } else if (modeStr == "subprocess") {
-                    config.mode = execConfig::SUBPROCESS;
-                } else {
-                    std::cerr << "Error: Invalid mode '" << modeStr << "'. Valid modes are: ws, cli, subprocess" << std::endl;
-                    std::cerr << hint << std::endl;
-                    return 1;
-                }
             } else {
                 std::cerr << "Error: Option " << arg << " requires an argument." << std::endl;
                 std::cerr << hint << std::endl;

29ba7433a86c74efef19a00616e8f51d4b6be9be
tab <2969117392@qq.com>
2026-02-12 01:58:20 +0800

    build: use different directories on Windows or Linux

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-mkdir build
-cd build
+mkdir build-win
+cd build-win
 cmake .. -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake"
 cmake --build . --config Release
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-mkdir build
-cd build
+mkdir build-linux
+cd build-linux
 cmake .. -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
 cmake --build .
